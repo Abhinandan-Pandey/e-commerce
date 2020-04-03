eea6b61176fa2b77cbb35862f51d8055013229fc
Abhinandan-Pandey <abhinandanpandey.ap@gmail.com>
2020-04-04 03:58:27 +0530

    Updates

---
+++ precache-manifest.ad98427aa2a2a69f2a7ee53f6afbf09d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba5cb6d3a6dc29062c5e1e16cf4ae19",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "2d2ce52a0c464604c567",
+    "url": "/e-commerce/static/css/main.98b18ff9.chunk.css"
+  },
+  {
+    "revision": "9545ebf4bd6ff49723ea",
+    "url": "/e-commerce/static/js/2.6f54cda7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/e-commerce/static/js/2.6f54cda7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d2ce52a0c464604c567",
+    "url": "/e-commerce/static/js/main.9b8a4d81.chunk.js"
+  },
+  {
+    "revision": "1dcf056994fcc929c1ad",
+    "url": "/e-commerce/static/js/runtime-main.2a529cf4.js"
+  }
+]);
